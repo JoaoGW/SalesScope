de1148e086f47d47a172b2c79d199782f957adde
João Ribeiro <cpsenha@gmail.com>
2024-09-26 22:11:34 -0300

    Reestruturacao basica das paginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import MenuLateral from "@/components/Menu/menuLateral";
-
 export default function HomeDashboard() {
   return (
     <div className="bg-white">
-      <MenuLateral/>
       <main>
         
       </main>
