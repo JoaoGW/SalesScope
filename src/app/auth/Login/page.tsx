0454887938fb49e9ee0fd3d5a63bcdd77c383250
João Ribeiro <cpsenha@gmail.com>
2024-09-30 13:16:11 -0300

    Finalizada a funcionalidade de Login utilizando OAuth2.0

--- src/app/auth/Login/page.tsx
+++ src/app/auth/Login/page.tsx
@@ -1,103 +1,29 @@
 "use client";
 import Link from "next/link";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { useEffect } from "react";
-import { getAuth } from "firebase/auth";
-import firebase from "firebase/compat/app";
-import * as firebaseui from "firebaseui";
-import "firebaseui/dist/firebaseui.css";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Login() {
+  const { user } = useUser();
 
-    useEffect(() => {
-        const ui =
-          firebaseui.auth.AuthUI.getInstance() ||
-          // since Firebase v9 and above service are imported when needed instad of being a namespace
-          new firebaseui.auth.AuthUI(getAuth(app));
-    
-        ui.start("#firebaseui-auth-container", {
-          signInSuccessUrl: "/dashboard/Home",
-          signInOptions: [
-            // Leave the lines as is for the providers you want to offer your users.
-            {
-              provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-              clientId:
-                "1006672558365-p6jpr3b7r76kng93j6mrirh9pua5k2at.apps.googleusercontent.com",
-            },
-            {
-              provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            },
-            // leave for ANOTHER video
-            // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
-          ],
-          // required to enable one-tap sign-up credential helper
-          credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
-        });
-      }, []);
+  useEffect(() => {
+    if (user) {
+      // Redirecionamento usando window.location
+      window.location.href = '/dashboard/Home';
+    }
+  }, [user]);
 
-    const loginSchema = z.object({
-        email: z.string().email({ message: "Please insert a valid email" }),
-        senha: z.string().min(8, { message: "Password should have at least 8 characters" }),
-    });
-
-    type LoginForm = z.infer<typeof loginSchema>;
-
-    const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>({
-        resolver: zodResolver(loginSchema),
-    });
-
-    const handleLogin = async (data: LoginForm) => {
-        try {
-            
-        } catch (error) {
-            let errorMessage = "Ocorreu um erro ao tentar fazer login. Tente novamente.";
-            if (error instanceof Error && "code" in error) {
-                switch ((error as any).code) {
-                    case "auth/wrong-password":
-                        errorMessage = "A senha digitada está incorreta.";
-                        break;
-                    case "auth/user-not-found":
-                        errorMessage = "Não encontramos um usuário com este e-mail.";
-                        break;
-                    case "auth/invalid-email":
-                        errorMessage = "O e-mail fornecido não é válido.";
-                        break;
-                    default:
-                        errorMessage = "Erro desconhecido. Tente novamente.";
-                        break;
-                }
-            }
-            console.error("Login failed:", error);
-        }
-    };
-
-    return (
-        <section className="flex flex-row justify-center items-center w-full h-screen px-32 py-20">
-            <div className="flex flex-row bg-blued-purple p-3 w-1/2 rounded-l-xl h-full">
-                <p>Let's sign you in</p>
-                <div className="flex flex-row">
-                    <p>Welcome to Sales Scope</p>
-                    <Link href={"./SignUp"}>Sign Up</Link>
-                </div>
-                <form onSubmit={handleSubmit(handleLogin)}>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input {...register("email")} className="" type="text" id="email" />
-                        {errors.email && <p>{errors.email.message}</p>}
-                    </div>
-                    <div>
-                        <label htmlFor="senha">Password</label>
-                        <input {...register("senha")} className="" type="password" id="senha" />
-                        {errors.senha && <p>{errors.senha.message}</p>}
-                    </div>
-                    <button type="submit">Login</button>
-                </form>
-                <StyledFirebaseAuth/>
-            </div>
-            <div className="bg-slate-200 p-3 w-1/2 rounded-r-xl h-full"></div>
-        </section>
-    );
-}
+  return (
+    <section className="flex flex-row justify-center items-center w-full h-screen px-32 py-20">
+      <div className="flex flex-row bg-blued-purple p-3 w-1/2 rounded-l-xl h-full">
+        <p>Let's sign you in</p>
+        <div className="flex flex-row">
+          <p>Welcome to Sales Scope</p>
+          <Link href="/SignUp">Sign Up</Link>
+        </div>
+      </div>
+      <a href="/api/auth/login">Login</a>
+      <div className="bg-slate-200 p-3 w-1/2 rounded-r-xl h-full"></div>
+    </section>
+  );
+}  
--- src/app/dashboard/Home/page.tsx
+++ src/app/dashboard/Home/page.tsx
@@ -1,11 +1,27 @@
+"use client"
 import MenuLateral from "@/components/Menu/menuLateral";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function HomeDashboard() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) { return <p>Loading...</p> }
+  if (error) { return <p>An error occurred with your login attempt</p> }
+
   return (
     <div>
-      <MenuLateral/>
-      <main>
-        
+      <MenuLateral />
+      <main className="ml-36">
+        {user && ( // Corrigido para renderizar corretamente
+          <div>
+            <p>Conteúdo Principal da Home</p>
+            <div>
+              <p>User: {user?.name}</p>
+              <p>Email: {user?.email}</p>
+            </div>
+            <a href="/api/auth/logout">Logout</a>
+          </div>
+        )}
       </main>
     </div>
   );
