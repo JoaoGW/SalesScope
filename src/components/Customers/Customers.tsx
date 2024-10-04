3ddce713d5b4fe40aa1b7a3df4ba4998889c37fb
João Ribeiro <cpsenha@gmail.com>
2024-10-04 12:02:56 -0300

    Added the reviews content to the Home Dashboard.

--- src/components/Customers/Customers.tsx
+++ src/components/Customers/Customers.tsx
@@ -1,6 +1,7 @@
 import { Suspense, useEffect, useState } from "react"
 import { LoadingSpinner } from "../LoadingSpinners/Spinners"
 import { CircleUser } from 'lucide-react';
+import { ArrowDown } from 'lucide-react';
 import StarRatings from 'react-star-ratings';
 
 interface Customer {
@@ -42,39 +43,53 @@ export function CustomersReviews() {
     const [customers, setCustomers] = useState<Customer[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [error, setError] = useState<string>("");
+    const [isLaptop, setIsLaptop] = useState<boolean>(false);
 
     const customersData = async () => {
         try {
-            // Fetch the data that the internal api gathers from the remote API
             const response = await fetch('/api/customers');
-            const data = await response.json();
-
-            // Assigning random reviews from 1 to 5 to each customers
-            const customersMod: Customer[] = data;
-            const modifiedCustomers = customersMod.map(customer => ({
-                ...customer,
-                lastRating: generateRandomReviewValue()
-            }))
-            setCustomers(modifiedCustomers);
+            const result = await response.json();
+        
+            // Log the original data
+            console.log('Dados originais dos clientes:', result);
+    
+            // Verifica se 'data' existe e é um array
+            if (Array.isArray(result.data)) {
+                const modifiedCustomers = result.data.map((customer: Customer) => ({
+                    ...customer,
+                    lastRating: generateRandomReviewValue(),
+                }));
+    
+                // Log the modified data
+                console.log('Dados modificados dos clientes:', modifiedCustomers);
+    
+                setCustomers(modifiedCustomers);
+            } else {
+                console.error('Formato inesperado de dados:', result);
+            }
         } catch (error) {
             setError((error as Error).message);
-        } finally{
+        } finally {
             setIsLoading(false);
         }
-    }
+    };        
 
+    // Effects without parameters
     useEffect(() => {
         customersData();
+
+        // Checking if is a Desktop Monitor Screen or a Laptop Screen
+        setIsLaptop(window.matchMedia('(max-width: 1919px)').matches);
     }, []);
 
     return (
         <Suspense fallback={<LoadingSpinner/>}>
-            <section className="flex flex-col w-5/6 items-center">
-                { customers.slice(0, 7).map((data) => (
-                    <div key={data.id} className="flex flex-col" id="userReview">
+            <section className="flex flex-col w-5/6 items-center mx-auto">
+                { isLaptop && customers.slice(0, 4).map((data) => (
+                    <div key={data.id} className="flex flex-col mb-5 w-full border-b-2 pb-3" id="userReview">
                         <div className="flex flex-row justify-between" id="reviewHeader">
-                            <div className="flex flex-row">
-                                <CircleUser/>
+                            <div className="flex flex-row items-center mb-2">
+                                <CircleUser className="mr-3" size={'45px'}/>
                                 <div className="flex flex-col">
                                     <p>{data.name.firstname + data.name.lastname}</p>
                                     <p>{data.email}</p>
@@ -85,13 +100,16 @@ export function CustomersReviews() {
                                     rating={generateRandomReviewValue()}
                                     starRatedColor="yellow"
                                     numberOfStars={5}
+                                    starDimension="20px"
+                                    starSpacing="3px"
                                 />
                                 <p>{generateRandomNumber()} min ago</p>
                             </div>
                         </div>
-                        <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing  </p>
+                        <p className="">"Lorem ipsum dolor sit amet, consectetur adipiscing"</p>
                     </div>
                 )) }
+                <button type="button" className="flex flex-row font-bold text-center">See All<ArrowDown className="ml-2"/></button>
             </section>
         </Suspense>
     )
